d640939a144ed14d6eb540f04419e7666beb2912
Adrian Rogut <adrian.rogut@gmail.com>
2017-07-18 22:12:26 +0200

    Initial commit

---
+++ src/main/java/com/arogut/shas/ShasApplication.java
@@ -0,0 +1,12 @@
+package com.arogut.shas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShasApplication.class, args);
+	}
+}
