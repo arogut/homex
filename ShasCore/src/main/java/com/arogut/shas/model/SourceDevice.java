d7849a6173a63daa7e4e947e1aa44323df3441da
Adrian Rogut <adrian.rogut@gmail.com>
2017-08-04 16:46:51 +0200

    Add execution to pom

--- ShasCore/src/main/java/com/arogut/shas/model/SourceDevice.java
+++ ShasCore/src/main/java/com/arogut/shas/model/SourceDevice.java
@@ -1,13 +1,15 @@
 package com.arogut.shas.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 import java.util.Map;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class SourceDevice extends Device {
     @Transient
     private Map<String, Object> measurements;
