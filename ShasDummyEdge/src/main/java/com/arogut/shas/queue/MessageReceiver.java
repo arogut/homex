e5e0705ca4b40e79ab29c5f7bfff0cfe0941ba84
Adrian Rogut <adrian.rogut@gmail.com>
2017-10-18 00:10:16 +0200

    Add simple rabbitmq communication

---
+++ ShasDummyEdge/src/main/java/com/arogut/shas/queue/MessageReceiver.java
@@ -0,0 +1,15 @@
+package com.arogut.shas.queue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageReceiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageReceiver.class);
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received message <<< {} >", message);
+    }
+}
