fe869fb7d3e65c1ca61a4d5f5619e0ce13df8545
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-28 00:41:08 +0200

    Fix issue

---
+++ shas-ui/src/main/java/com/arogut/shas/ShasUIApplication.java
@@ -0,0 +1,12 @@
+package com.arogut.shas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShasUIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShasUIApplication.class, args);
+	}
+}
