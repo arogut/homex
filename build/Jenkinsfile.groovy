2f92095dbdb957b1ab4bf67ea16a553160f8f3e1
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-29 00:14:21 +0200

    Update Jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -5,6 +5,13 @@ try {
 //                git url: "${GIT_SOURCE_URL}", branch: "${GIT_SOURCE_REF}"
 //            }
             stage("Build") {
+                def userInput = input(
+                        id: 'userInput', message: 'Let\'s promote?', parameters: [
+                        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
+                        [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
+                ])
+                echo ("Env: "+userInput['env'])
+                echo ("Target: "+userInput['target'])
                 sh "mvn -f ${env.WORKSPACE}/pom.xml clean verify"
             }
         }
