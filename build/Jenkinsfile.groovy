082d4b11f7ae72989316ca440606f98df3f3eede
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-29 00:37:46 +0200

    Update Jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -1,17 +1,10 @@
 try {
     timeout(time: 20, unit: 'MINUTES') {
         node("maven") {
-//            stage("Checkout") {
-//                git url: "${GIT_SOURCE_URL}", branch: "${GIT_SOURCE_REF}"
-//            }
+            stage("Checkout") {
+                git url: "${GIT_SOURCE_URL}", branch: "${GIT_SOURCE_REF}"
+            }
             stage("Build") {
-                def userInput = input(
-                        id: 'userInput', message: 'Let\'s promote?', parameters: [
-                        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
-                        [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
-                ])
-                echo ("Env: "+userInput['env'])
-                echo ("Target: "+userInput['target'])
                 sh "mvn -f ${env.WORKSPACE}/pom.xml clean verify"
             }
         }
