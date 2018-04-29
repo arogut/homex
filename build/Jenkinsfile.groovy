25a80d6dce2f187b2648f143ec11070f86887bae
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-30 00:02:19 +0200

    Update Jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 try {
     timeout(time: 20, unit: 'MINUTES') {
+        appVersion = version()
         node("maven") {
             stage("Checkout") {
                 checkout scm
@@ -9,7 +10,8 @@ try {
             }
             stage("Build Image") {
 //              build device registry TODO: change it
-                sh "oc start-build shas-device-registry-docker --from-file=shas-device-registry/target/ShasDeviceRegistry-0.0.1-SNAPSHOT.jar -n shas-int"
+                sh "oc start-build shas-device-registry-docker --from-file=build/shas-device-registry/Dockerfile -n shas-int"
+                sh "cp shas-device-registry/target/ShasDeviceRegistry-${appVersion}-exec.jar build/shas-device-registry/app.jar"
             }
             stage("Deploy") {
                 openshiftDeploy deploymentConfig: "shas-device-registry", namespace: "shas-int"
@@ -21,4 +23,9 @@ try {
     echo "Caught: ${err}"
     currentBuild.result = 'FAILURE'
     throw err
+}
+
+def version() {
+    def matcher = readFile('pom.xml') =~ '<version>(.+?)</version>'
+    matcher ? matcher[0][1] : null
 }
