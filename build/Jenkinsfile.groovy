f166d01e1b5f252e54c58da05fc6b1fb9f3add7e
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-29 22:25:25 +0200

    Update Jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -7,6 +7,13 @@ try {
             stage("Build") {
                 sh "mvn clean verify"
             }
+            stage("Build Image") {
+//              build device registry TODO: change it
+                sh "oc start-build shas-device-registry-docker --from-file=shas-device-registry/target/ShasDeviceRegistry-0.0.1-SNAPSHOT.jar -n shas-int"
+            }
+            stage("Deploy") {
+                openshiftDeploy deploymentConfig: "shas-device-registry", namespace: "shas-int"
+            }
         }
     }
 } catch (err) {
