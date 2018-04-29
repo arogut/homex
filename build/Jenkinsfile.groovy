a22958bee081bef9937e2461f0fa60e1978ef1af
Adrian Rogut <adrian.rogut@gmail.com>
2018-04-30 00:05:57 +0200

    Update Jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 try {
     timeout(time: 20, unit: 'MINUTES') {
-        def appVersion = version()
         node("maven") {
             stage("Checkout") {
                 checkout scm
@@ -9,6 +8,7 @@ try {
                 sh "mvn clean verify"
             }
             stage("Build Image") {
+                def appVersion = version()
 //              build device registry TODO: change it
                 sh "oc start-build shas-device-registry-docker --from-file=build/shas-device-registry/Dockerfile -n shas-int"
                 sh "cp shas-device-registry/target/ShasDeviceRegistry-${appVersion}-exec.jar build/shas-device-registry/app.jar"
