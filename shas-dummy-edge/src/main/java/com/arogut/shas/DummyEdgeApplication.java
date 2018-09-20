ad24e3c8676982a73118f6b0b70e7ba8ada9ec5e
Adrian Rogut <adrian.rogut@gmail.com>
2018-09-20 22:13:06 +0200

    Remove amqp for now

--- shas-dummy-edge/src/main/java/com/arogut/shas/DummyEdgeApplication.java
+++ shas-dummy-edge/src/main/java/com/arogut/shas/DummyEdgeApplication.java
@@ -1,38 +1,20 @@
 package com.arogut.shas;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Component
 public class DummyEdgeApplication implements CommandLineRunner {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(DummyEdgeApplication.class);
+    @Override
+    public void run(String... strings) throws Exception {
+    }
 
-	private final RabbitTemplate rabbitTemplate;
-	private final ConfigurableApplicationContext context;
-
-	public DummyEdgeApplication(RabbitTemplate rabbitTemplate,
-                                ConfigurableApplicationContext context) {
-		this.rabbitTemplate = rabbitTemplate;
-		this.context = context;
-	}
-
-	@Override
-	public void run(String... strings) throws Exception {
-		LOGGER.info("Sending message >>>>");
-		rabbitTemplate.convertAndSend("message-in", "{}");
-		context.close();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(DummyEdgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DummyEdgeApplication.class, args);
+    }
 
 }
