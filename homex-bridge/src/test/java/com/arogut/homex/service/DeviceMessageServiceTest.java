9e09a85175063d6a0f4b434fe96cec497dec85ed
Adrian Rogut <adrian.rogut@gmail.com>
2020-01-08 22:46:04 +0100

    Add missing device messsage tests

---
+++ homex-bridge/src/test/java/com/arogut/homex/service/DeviceMessageServiceTest.java
@@ -0,0 +1,54 @@
+package com.arogut.homex.service;
+
+import com.arogut.homex.model.DeviceMessage;
+import com.arogut.homex.model.Measurement;
+import org.assertj.core.api.Assertions;
+import org.influxdb.InfluxDB;
+import org.influxdb.dto.Point;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+@ExtendWith(MockitoExtension.class)
+class DeviceMessageServiceTest {
+
+    @Mock
+    private InfluxDB influxDB;
+
+    @InjectMocks
+    private DeviceMessageService deviceMessageServiceService;
+
+    @Test
+    void shouldReturnAllDevicesFromDB() {
+        DeviceMessage msg = DeviceMessage.builder()
+                .deviceId("dummy")
+                .measuredTime(Instant.now().toEpochMilli())
+                .receivedTime(Instant.now().toEpochMilli())
+                .data(List.of(Measurement.builder()
+                        .name("temp")
+                        .value("25").build()
+                ))
+                .build();
+
+        deviceMessageServiceService.handle(msg);
+
+        ArgumentCaptor<Point> pointCaptor = ArgumentCaptor.forClass(Point.class);
+        Mockito.verify(influxDB).write(pointCaptor.capture());
+        Assertions.assertThat(pointCaptor.getValue()).isEqualTo(
+                Point.measurement("measurement")
+                        .time(msg.getMeasuredTime(), TimeUnit.MILLISECONDS)
+                        .addField("temp", "25")
+                        .build()
+        );
+    }
+}
+
